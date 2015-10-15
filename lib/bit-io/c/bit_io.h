481c4a7aeab51a026265b729daeda44c78ca50d7
Jesse A. Tov <jesse.tov@gmail.com>
2015-10-15 18:56:42 -0500

    docs for c and c++

--- lib/bit-io/c/bit_io.h
+++ lib/bit-io/c/bit_io.h
@@ -1,3 +1,13 @@
+/*
+ * bit_io.h: facilities for reading and writing files one bit (or more)
+ * at a time.
+ *
+ * The main idea is that we create a bit input or output stream as a
+ * layer over a file open for reading or writing. The library maintains
+ * a buffer of bits so that the client can view the file as a simple
+ * sequence of bits rather than bytes.
+ */
+
 #ifndef BIT_IO_H_EECS214
 #define BIT_IO_H_EECS214
 
@@ -5,68 +15,186 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef uint8_t byte;
+/*
+ * Abstract type of bit input streams
+ */
 typedef struct bit_in_t *bit_in;
+
+/*
+ * Abstract type of bit output streams
+ */
 typedef struct bit_out_t *bit_out;
 
 /*
- * Overlays a bit filter for reading from an open file handle. The
- * result of interleaving operations on the underlying file handle with
- * operations on the bit filter is unspecified.
+ * Creates a bit input stream that reads from the given open file
+ * handle.
+ *
+ * The result of interleaving operations on the underlying file
+ * handle with operations on the bit stream is unspecified.
+ *
+ * Parameters:
+ *
+ *      f - input file handle to read from
+ *
+ * Returns: a bit input stream handle, or NULL if memory cannot be
+ * allocated
+ *
+ * Example:
+ *
+ *      bit_in bin = b_attach_in(fopen(input_file_name, "r"));
+ *
  */
 bit_in b_attach_in(FILE *f);
 
 /*
- * Overlays a bit filter for writing to an open file handle. The result
- * of interleaving operations on the underlying file handle with
- * operations on the bit filter is unspecified.
+ * Creates a bit output stream that writes to the given open file
+ * handle.
+ *
+ * The result of interleaving operations on the underlying file
+ * handle with operations on the bit stream is unspecified.
+ *
+ * Parameters:
+ *
+ *      f - output file handle to write to
+ *
+ * Returns: a bit output stream handle, or NULL if memory cannot be
+ * allocated
+ *
+ * Example:
+ *
+ *      bit_out bout = b_attach_out(fopen(input_file_name, "w"));
  */
 bit_out b_attach_out(FILE *f);
 
 /*
- * Writes one bit to the bit filter. Returns the number of bytes written
- * to the bit filter's underlying file, or `EOF` on error or
- * end-of-file.
- */
-int b_write_bit(bool bit, bit_out bf);
-
-/*
- * Reads one bit from the bit filter. Returns 0 for false, 1 for true,
- * or `EOF` on failure or end-of-file.
+ * Reads one bit from a bit input stream.
+ *
+ * Parameters:
+ *
+ *      bf - the bit input stream to read from
+ *
+ * Returns: 0, 1, or `EOF` on failure or end-of-file.
+ *
+ * Example:
+ *
+ *      int result = b_read_bit(bin);
+ *      if (result == EOF) {
+ *          // end-of-file
+ *      } else {
+ *          // result is the bit, 0 or 1
+ *      }
  */
 int b_read_bit(bit_in bf);
 
 /*
- * Writes the low-order `nbits` bits of `bits` to the bit filter. Or in
- * other words, writes `bits` as a big-endian `nbits`-bit number.
- * Returns the number of bytes written to the bit filter's underlying
- * file, or `EOF` on error or end-of-file.
+ * Writes one bit to a bit output stream.
+ *
+ * Parameters:
+ *
+ *      bit - the bit to write
+ *      bf - the bit output stream to write to
+ *
+ * Returns: 0 for success, or `EOF` on error or end-of-file.
+ *
+ * Example:
+ *
+ *      if (b_write_bit(true, bout) == EOF) {
+ *          // write failed
+ *      }
  */
-int b_write_bits(int bits, size_t nbits, bit_out bf);
+int b_write_bit(bool bit, bit_out bf);
 
 /*
- * Reads `nbits` bits from the bit filter into the low-order `nbits`
- * bits of `bits`. Or in other words, reads `bits` as a big-endian
- * `nbits`-bit number. Returns 0 for success or `EOF` on failure or
- * end-of-file.
+ * Reads `nbits` bits from the bit input stream into the low-order
+ * `nbits` bits of `bits`. Or in other words, reads `bits` as a
+ * big-endian `nbits`-bit number.
+ *
+ * Parameters:
+ *
+ *      bits - pointer to store the result to
+ *      nbits - number of bits to read
+ *      bf - bit input stream to read from
+ *
+ * Returns: 0 for success, or `EOF` on failure or end-of-file.
+ *
+ * Example:
+ *
+ *      int result;
+ *      if (b_read_bits(&result, 5, bin) == OEF) {
+ *          // end-of-file or read error
+ *      } else {
+ *          // result contains the five-bit number that was read
+ *      }
  */
 int b_read_bits(int *bits, size_t nbits, bit_in bf);
 
 /*
- * Returns true if there are no more bits to read. Note that because
+ * Writes the low-order `nbits` bits of `bits` to the bit output stream.
+ * Or in other words, writes `bits` as a big-endian `nbits`-bit number.
+ *
+ * Parameters:
+ *
+ *      bits - number to write
+ *      nbits - number of bits to write
+ *      bf - bit output stream to write to
+ *
+ * Returns: 0 for success, or `EOF` on failure or end-of-file.
+ *
+ * Example:
+ *
+ *      if (b_write_bits(22, 5, bout) == OEF) {
+ *          // end-of-file or read error
+ *      } else {
+ *          // wrote 10110 to stream
+ *      }
+ */
+int b_write_bits(int bits, size_t nbits, bit_out bf);
+
+/*
+ * Determines whether there are no more bits to read. Note that because
  * files store bits in octets (bytes), the number of bits read will be a
- * multiple of 8, even if that exceeds the number written.
+ * multiple of 8, even if that exceeds the number explicitly written.
+ *
+ * Parameters:
+ *
+ *      bf - the bit input stream to check
+ *
+ * Returns: non-zero if there are no more bits to read, and zero if
+ * there are
+ *
+ * Example:
+ *
+ *      while (! b_eof(bin)) {
+ *          int b = b_read_bit(bin);
+ *          ...
+ *      }
  */
 int b_eof(const bit_in bf);
 
 /*
- * Disposes of a bit filter.
+ * Disposes of a bit input stream, returning its resources to the system.
+ *
+ * Parameters:
+ *
+ *      bf - the bit input stream to close
+ *
+ * Example:
+ *
+ *      b_detach_in(bin);
  */
 void b_detach_in(bit_in bf);
 
 /*
- * Disposes of a bit filter, padding with 0s the remaining bits to the
- * nearest full byte.
+ * Disposes of a bit output stream, padding with 0s the remaining bits to
+ * the nearest full byte and returning its resources to the system.
+ *
+ * Parameters:
+ *
+ *      bf - the bit output stream to close
+ *
+ * Example:
+ *
+ *      b_detach_out(bout);
  */
 void b_detach_out(bit_out bf);
 
--- lib/bit-io/cpp/bit_io.hpp
+++ lib/bit-io/cpp/bit_io.hpp
@@ -1,5 +1,15 @@
 #pragma once
 
+/*
+ * bit_io.hpp: classes for reading and writing files one bit (or more)
+ * at a time.
+ *
+ * The main idea is that we create a bit input or output stream as a
+ * layer over a file that we open for reading or writing. The library
+ * maintains a buffer of bits so that the client can view the file as a
+ * simple sequence of bits rather than bytes.
+ */
+
 #include <istream>
 #include <ostream>
 #include <fstream>
@@ -9,17 +19,99 @@ namespace eecs214 {
      * INPUT
      */
 
+    // A bit input stream, for reading individual bits from a file.
     class bifstream {
       public:
+        // Constructs a bit input stream to read from the given file.
+        //
+        // Parameters:
+        //
+        //      filespec - name of the file to open
+        //
+        // Example:
+        //
+        //      bifstream bif(input_file_name);
+        //
         explicit bifstream(const char* filespec);
 
-        bifstream& read(bool&);
+        // Reads a bit from this bit input stream into the given bool
+        // reference.
+        //
+        // Parameters:
+        //
+        //      result - bool reference to store the bit that was read
+        //
+        // Returns: the same bit input stream, for method chaining
+        //
+        // Example:
+        //
+        //      bool b;
+        //      bif.read(b);
+        //
+        bifstream& read(bool& result);
 
+        // Reads `n` bits, interpreted as a big-endian `n`-bit integer,
+        // and stores them in the reference `result`, which must have a
+        // numeric type.
+        //
+        // Parameters:
+        //
+        //      <class T> - the type of the result; must be numeric
+        //      result - reference for storing the result
+        //      n - number of bits to read
+        //
+        // Returns: the same bit input stream, for method chaining
+        //
+        // Example:
+        //
+        //      bool b;
+        //      bif.read(b);
+        //
         template <class T>
         bifstream& read_bits(T& result, size_t n);
 
+        // Determines whether we've reached the end of the input file.
+        //
+        // Returns: `true` if there are no more bits to read and `false`
+        // if there are
+        //
+        // Example:
+        //
+        //      while (! bif.eof()) {
+        //          bif.read(b);
+        //          ...
+        //      }
+        //
         bool eof() const;
+
+        // Determines the status of the bit input stream.
+        //
+        // Returns: `false` if there are no bits to read and a read
+        // error has occurred, and `true` otherwise.
+        //
+        // Example:
+        //
+        //      bif.read(b);
+        //      if (bif.good()) {
+        //          // we know the read succeeded
+        //      }
+        //
         bool good() const;
+
+        // The bit input stream boolean coercion operator, inserted, for
+        // example, when a `bifstream` is used as a condition for an
+        // `if`. Alias for `good()`.
+        //
+        // Returns: `false` if there are no bits to read and a read
+        // error has occurred, and `true` otherwise.
+        //
+        // Example:
+        //
+        //      bif.read(b);
+        //      if (bif) {
+        //          // we know the read succeeded
+        //      }
+        //
         operator bool() const;
 
         bifstream(const bifstream&) = delete;
@@ -29,6 +121,133 @@ namespace eecs214 {
         short nbits;
         std::ifstream stream;
     };
+
+    // The bit input stream extraction operator; alias for `read(bool&)`.
+    //
+    // Params:
+    //
+    //     - bif - the bit input stream to read from
+    //     - b - reference to store the bit that was read
+    //
+    // Returns: the same bit input stream, for method chaining
+    //
+    // Example:
+    //
+    //     bool b1, b2, b3;
+    //     bif >> b1 >> b2 >> b3;
+    //
+    bifstream& operator>>(bifstream& bif, bool& b);
+
+    /*
+     * OUTPUT
+     */
+
+    // A bit output stream, for writing individual bits to a file.
+    class bofstream {
+      public:
+        // Constructs a bit output stream to write to the given file.
+        //
+        // Parameters:
+        //
+        //      filespec - name of the file to open or create
+        //
+        // Example:
+        //
+        //      bofstream bof(output_file_name);
+        //
+        explicit bofstream(const char* filespec);
+
+        // Writes a bit to this bit output stream.
+        //
+        // Parameters:
+        //
+        //      b - the bit to write
+        //
+        // Returns: the same bit output stream, for method chaining
+        //
+        // Example:
+        //
+        //      bof.write(true);
+        //
+        bofstream& write(bool b);
+
+        // Writes an `n`-bit big-endian representation of `value`, which
+        // must have a numeric type, to this bit output sream.
+        //
+        // Parameters:
+        //
+        //      <class T> - the type of the value to write; must be numeric
+        //      value - the value to write
+        //      n - number of bits to write
+        //
+        // Returns: the same bit output stream, for method chaining
+        //
+        // Example:
+        //
+        //      bof.write(22, 5); // writes 10110
+        //      bof.write(22, 6); // writes 010110
+        //
+        template <class T>
+        bofstream& write_bits(T value, size_t n);
+
+        // Determines the status of the bit output stream.
+        //
+        // Returns: `false` if an error has occurred; `true` otherwise
+        //
+        // Example:
+        //
+        //      bof.write(b);
+        //      if (bof.good()) {
+        //          // we know the write succeeded
+        //      }
+        //
+        bool good() const;
+
+        // The bit output stream boolean coercion operator, inserted, for
+        // example, when a `bofstream` is used as a condition for an
+        // `if`. Alias for `good()`.
+        //
+        // Returns: `false` if an error has occurred; `true` otherwise
+        //
+        // Example:
+        //
+        //      bof.write(b);
+        //      if (bof) {
+        //          // we know the write succeeded
+        //      }
+        //
+        operator bool() const;
+
+        ~bofstream();
+
+        bofstream(const bofstream&) = delete;
+
+      private:
+        char bitbuf;
+        short nbits;
+        std::ofstream stream;
+        void write_out();
+    };
+
+    // The bit output stream insertion operator; alias for
+    // `write(bool)`.
+    //
+    // Params:
+    //
+    //     - bof - the bit input stream to write to
+    //     - b - the bit to write
+    //
+    // Returns: the same bit output stream, for method chaining
+    //
+    // Example:
+    //
+    //     bof << true << false << false;
+    //
+    bofstream& operator<<(bofstream& bof, bool b);
+
+    /*
+     * TEMPLATE IMPLEMENTATIONS
+     */
 
     template <class T>
     bifstream& bifstream::read_bits(T& result, size_t n)
@@ -50,38 +269,11 @@ namespace eecs214 {
         return bif.read(bit);
     }
 
-    /*
-     * OUTPUT
-     */
-
-    class bofstream {
-      public:
-        explicit bofstream(const char* filespec);
-
-        bool good() const;
-        operator bool() const;
-
-        bofstream& write(bool);
-
-        template <class T>
-        bofstream& write_bits(T buf, size_t n);
-
-        ~bofstream();
-
-        bofstream(const bofstream&) = delete;
-
-      private:
-        char bitbuf;
-        short nbits;
-        std::ofstream stream;
-        void write_out();
-    };
-
     template <class T>
-    bofstream& bofstream::write_bits(T buf, size_t n)
+    bofstream& bofstream::write_bits(T value, size_t n)
     {
         while (n--) {
-            write(buf >> n & 1);
+            write(value >> n & 1);
         }
 
         return *this;
