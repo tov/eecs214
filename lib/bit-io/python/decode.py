804d8b0aae22e5ae5cc4e83b1dab77ae3a653623
Jesse Tov <jesse.tov@gmail.com>
2015-10-21 20:29:42 -0500

    changed encode.py and decode.py to use binary mode for plaintext.

--- lib/bit-io/python/decode.py
+++ lib/bit-io/python/decode.py
@@ -7,7 +7,7 @@ if len(argv) != 3:
     stderr.write('Usage: {} INFILE OUTFILE\n'.format(argv[0]))
     exit(2)
 
-with bit_io.BitReader(argv[1]) as input, open(argv[2], 'w') as output:
+with bit_io.BitReader(argv[1]) as input, open(argv[2], 'wb') as output:
     while True:
         c = input.readbits(7)
         if not c: break
--- lib/bit-io/python/encode.py
+++ lib/bit-io/python/encode.py
@@ -7,7 +7,7 @@ if len(argv) != 3:
     stderr.write('Usage: {} INFILE OUTFILE\n'.format(argv[0]))
     exit(2)
 
-with open(argv[1], 'r') as input, bit_io.BitWriter(argv[2]) as output:
+with open(argv[1], 'rb') as input, bit_io.BitWriter(argv[2]) as output:
     while True:
         c = input.read(1)
         if not c: break
