647ba946219102f138bbee68661f9865f9dc5f57
Jesse Tov <jesse.tov@gmail.com>
2015-11-23 22:43:51 -0600

    cleaned up C lib

--- lib/bit-io/c/decode.c
+++ lib/bit-io/c/decode.c
@@ -1,17 +1,43 @@
 #include "bit_io.h"
 #include <stdio.h>
 
+/* These are the default files to read from and write to when no
+ * command-line arguments are given: */
+#define DEFAULT_INFILE  "hamlet.txt.huff"
+#define DEFAULT_OUTFILE "hamlet.txt.puff"
+
 void decode(FILE* in, FILE* out);
-int usage(const char** argv);
 
-int main(int argc, const char** argv)
+int main(int argc, const char* argv[])
 {
-    if (argc != 3) return usage(argv);
+    const char *infile, *outfile;
 
-    FILE* in = fopen(argv[1], "r");
+    switch (argc) {
+    // If there are two command-line arguments then they are the files
+    // to read from and write to:
+    case 3:
+        infile = argv[1];
+        outfile = argv[2];
+        break;
+
+    // If there are no command-line arguments then we use the default
+    // input and output files:
+    case 1:
+        infile = DEFAULT_INFILE;
+        outfile = DEFAULT_OUTFILE;
+        break;
+
+    // Any other number of arguments is a user error, so we print a help
+    // message:
+    default:
+        fprintf(stderr, "Usage: %s [ INFILE OUTFILE ]\n", argv[0]);
+        return 1;
+    }
+
+    FILE* in = fopen(infile, "r");
     if (!in) goto in_fail;
 
-    FILE* out = fopen(argv[2], "w");
+    FILE* out = fopen(outfile, "w");
     if (!out) goto out_fail;
 
     decode(in, out);
@@ -39,11 +65,4 @@ void decode(FILE *in, FILE *out)
     }
 
     b_detach_in(bf);
-}
-
-int usage(const char** argv)
-{
-    fprintf(stderr, "Usage: %s INFILE OUTFILE\n", argv[0]);
-
-    return 2;
 }
